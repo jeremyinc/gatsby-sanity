b7c1bf8516f119b44be2c56bbdfb4c1826002b6f
Jeremy Barnes <jeremy@wovenmagazine.com>
2020-01-18 17:23:26 -0800

    new live updates.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const path = require("path")
+require("dotenv").config({ path: path.resolve(`.env.${process.env.NODE_ENV}`) })
+
 module.exports = {
   /* Your site config here */
   plugins: [
@@ -12,9 +15,11 @@ module.exports = {
       options: {
         projectId: "c7kmfab9",
         dataset: "production",
+        watchMode: true,
+        overlayDrafts: true,
         // a token with read permissions is required
         // if you have a private dataset
-        // token: process.env.MY_SANITY_TOKEN,
+        token: process.env.MY_SANITY_TOKEN,
       },
     },
     `gatsby-plugin-sharp`,
